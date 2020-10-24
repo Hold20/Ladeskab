d7b8dcefa15cbea9b0154b830d6acddb8f4a45d4
Mehran Nabipour <201610403@post.au.dk>
2020-10-24 21:18:03 +0200

    tilføjet IRfidReader

---
+++ Ladeskab/IRfidReader.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ladeskab
+{
+
+    public class RfidEventArgs : EventArgs
+    {
+        public bool Rfid { get; }
+    }
+
+
+
+    public interface IRfidReader
+    {
+        void RfidDetected(id);
+
+        event EventHandler<RfidEventsArgs> RfidEvent;
+
+        protected virtual void RfidDetectedEvent(doorOpenEventArgs);
+    }
+}
