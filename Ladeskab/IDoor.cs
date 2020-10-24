dd59d89fe6090c6b6302f055a278b228c9ea98db
Mehran Nabipour <201610403@post.au.dk>
2020-10-24 19:00:45 +0200

    Add project files.

---
+++ Ladeskab/IDoor.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ladeskab
+{
+    public interface IDoor
+    {
+        void UnlockDoor();
+
+        void LockDoor();
+    }
+}
