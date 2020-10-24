fd5e6435804a95f0814eefe17f7ee79ebb1c4f27
Mehran Nabipour <201610403@post.au.dk>
2020-10-24 20:52:36 +0200

    3t

--- Ladeskab/IDoor.cs
+++ Ladeskab/IDoor.cs
@@ -1,9 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Dynamic;
 using System.Text;
 
 namespace Ladeskab
 {
+
+    public class doorOpenEventArgs : EventArgs
+    {
+        public bool door { get; }
+    }
+
+    public class doorCloseEventArgs : EventArgs
+    {
+        public bool door { get; }
+    }
+
+
     public interface IDoor
     {
         void lockedDoor();
@@ -18,6 +31,6 @@ namespace Ladeskab
 
         protected virtual void DoorOpened(doorOpenEventArgs);
 
-        protected virtual void DoorClosed(doorCloseEventsArgs);
+        protected virtual void DoorClosed(doorCloseEventArgs);
     }
 }
